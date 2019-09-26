f268d166b1aac6c0bc363e4568f98db1104e8237
Koh Wei Jie <contact@kohweijie.com>
2019-09-26 22:40:03 +0800

    changed to 12 levels for semaphore

--- contracts/ts/compileAndDeploy.ts
+++ contracts/ts/compileAndDeploy.ts
@@ -108,7 +108,7 @@ const compileAndDeploy = async (
     const semaphoreAbi = readFile(abiDir, 'Semaphore.abi')
     const semaphoreBin = readFile(abiDir, 'Semaphore.bin')
     const semaphoreContractFactory = new ethers.ContractFactory(semaphoreAbi, semaphoreBin, wallet)
-    const semaphoreContract = await semaphoreContractFactory.deploy(15, 0, 0)
+    const semaphoreContract = await semaphoreContractFactory.deploy(12, 0, 0)
     await semaphoreContract.deployed()
 
     console.log('Deployed Semaphore at', semaphoreContract.address)
